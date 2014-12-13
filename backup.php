608758be369c4108cf2e6dce0bf4354de230675e
YoDo <YoDo@github.com>
2014-12-14 07:06:53 +0800

    backup unlink

--- backup.php
+++ backup.php
@@ -1,14 +1,5 @@
 <?php
-	$filename = dirname(__FILE__).'/data.txt';
-	$finalModify = isset($_GET['timestamp']) ? $_GET['timestamp'] : 0;
-	$nowModify = filemtime($filename);
-	while($nowModify <= $finalModify){
-		usleep(10000);
-		clearstatcache();
-		$nowModify = filemtime($filename);
-	}
-	$response = array();
-	$response['msg'] = file_get_contents($filename);
-	$response['timestamp'] = $nowModify;
-	echo $response;
+	$filename = dirname(__FILE__).'/.data';
+	print file_get_contents($filename);
+//	unlink($filename);
 ?>
