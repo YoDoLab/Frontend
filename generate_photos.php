bd0fdc75d3e7d5a534bb4c97e95a1e5b33748bd5
YoDo <YoDo@github.com>
2014-12-14 07:27:57 +0800

    fix newline

--- generate_photos.php
+++ generate_photos.php
@@ -3,17 +3,15 @@
 	$photo_data = $_POST['data']['photos']['data'];
 	$name_list = $_POST['name'];
 
-	$result = array();
-	$i = 0;
 	foreach($photo_data as $event) {
 		if(array_key_exists($event['from']['name'], $name_list)) {
-			file_put_contents($filename, $event['source'] . "\n", FILE_APPEND);
+			file_put_contents($filename, $event['source'] . "\r\n", FILE_APPEND);
 			continue;
 		}
 
 		foreach($event['tags']['data'] as $tag) {
 			if(array_key_exists($tag['name'], $name_list)) {
-				file_put_contents($filename, $event['source'] . "\n", FILE_APPEND);
+				file_put_contents($filename, $event['source'] . "\r\n", FILE_APPEND);
 				break;
 			}
 		}
