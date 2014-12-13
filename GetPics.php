dab060c6a8c0d5199bbf171d5107ee4d2824d6b1
YoDo <YoDo@github.com>
2014-12-14 00:01:57 +0800

    delete comment

--- GetPics.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$data = $_POST['data'];
-	
-//	print_r(json_encode($data,JSON_UNESCAPED_UNICODE));
-//	print_r($data);
-//		print_r(json_decode($event['picture']));
-		echo $data['picture']['data']['url'];
-?>
---
+++ get_profile_pics.php
@@ -0,0 +1,3 @@
+<?php
+	print $_POST['data']['picture']['data']['url'];
+?>
