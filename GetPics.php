03982aa5af72d3c8a6b537f02d5e1a338fa87ec2
YoDo <YoDo@github.com>
2014-12-13 23:59:01 +0800

    update get pics

--- GetPics.php
+++ GetPics.php
@@ -1,5 +1,8 @@
 <?php
 	$data = $_POST['data'];
-	print_r($data);
-
+	
+//	print_r(json_encode($data,JSON_UNESCAPED_UNICODE));
+//	print_r($data);
+//		print_r(json_decode($event['picture']));
+		echo $data['picture']['data']['url'];
 ?>
